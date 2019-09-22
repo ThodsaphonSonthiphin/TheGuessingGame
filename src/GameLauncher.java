347da0c45092ae0c86f2920aecda23590064a552
thodsaphonsonthiphin <thodsaphonsp@gmail.com>
2019-09-22 11:35:04 +0700

    Initial commit

---
+++ src/GameLauncher.java
@@ -0,0 +1,27 @@
+public class GameLauncher {
+    public static void main(String[] args) {
+
+    }
+}
+
+class GuessGame{
+    Player p1;
+    Player p2;
+    Player p3;
+
+    void startGame(){
+        p1 = new Player();
+        p2 = new Player();
+        p3 = new Player();
+
+
+
+    }
+}
+
+class Player{
+    int number;
+    void guess(){}
+
+}
+
